2a416094a0ad44f78ccb44424d27b7235b4b59de
katratzi <ric_clifford@hotmail.com>
2024-02-18 17:00:11 +0800

    first assignment - creating some components

---
+++ 01 - getting started/my-first-app/src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'warning-alert',
+    templateUrl: './warningalert.component.html',
+    styles: `
+    h1 {
+        background-color: red
+    }
+    `
+})
+
+
+export class WarningAlert {
+
+}
