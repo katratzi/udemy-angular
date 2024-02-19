b3868d8f6340adffed95b74bb7670d98b78db753
katratzi <ric_clifford@hotmail.com>
2024-02-19 13:43:11 +0800

    combining data binding

--- 01 - getting started/my-first-app/src/app/servers/servers.component.ts
+++ 01 - getting started/my-first-app/src/app/servers/servers.component.ts
@@ -12,7 +12,7 @@ export class ServersComponent {
 
   allowNewServer = false
   serverCreationStatus = 'No server was created';
-  serverName = 'Test Server';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -21,7 +21,7 @@ export class ServersComponent {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created'
+    this.serverCreationStatus = 'Server was created with name ' + this.serverName
   }
 
   onUpdateServerName(event: any) {
