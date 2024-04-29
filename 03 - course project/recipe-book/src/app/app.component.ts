3f44418e0a10543a8dcd1702abc7a6c317298666
katratzi <ric_clifford@hotmail.com>
2024-04-29 13:33:06 +0800

    binding to select which feature to show with ngIf

--- 03 - course project/recipe-book/src/app/app.component.ts
+++ 03 - course project/recipe-book/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-book';
+  loadedFeature: string = 'recipes'
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature
+  }
 }
