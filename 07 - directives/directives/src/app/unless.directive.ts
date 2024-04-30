741eb69209d3bb31d773b97d62c6bfdce43651d1
katratzi <ric_clifford@hotmail.com>
2024-05-01 02:33:13 +0800

    building a structural directive

---
+++ 07 - directives/directives/src/app/unless.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appUnless]'
+})
+export class UnlessDirective {
+
+  // setter of the property
+  @Input() set appUnless(condition: boolean) {
+    if (!condition) {
+      // show the given template
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      // remove everything from this place in the dom
+      this.vcRef.clear()
+    }
+  }
+
+  //  get the what and where
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) {
+
+  }
+
+}
