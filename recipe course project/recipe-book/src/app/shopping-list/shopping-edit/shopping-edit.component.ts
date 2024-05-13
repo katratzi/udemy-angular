022a5158550dceed666142680ddc5d3e9e0821f3
katratzi <ric_clifford@hotmail.com>
2024-05-13 09:26:54 +0100

    shoping list service setup

--- recipe course project/recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe course project/recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -11,15 +12,16 @@ export class ShoppingEditComponent {
   @ViewChild('nameInput', { static: true }) nameInputRef: ElementRef
   @ViewChild('amountInput', { static: true }) amountInputRef: ElementRef
 
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  constructor(private shoppingListService: ShoppingListService) { }
 
   onAdd() {
 
+    // create new ingredient
     const name = this.nameInputRef.nativeElement.value;
     const amount = Number(this.amountInputRef.nativeElement.value);
-
     const newIngredient = new Ingredient(name, amount);
-    this.ingredientAdded.emit(newIngredient);
+    // add
+    this.shoppingListService.addIngredient(newIngredient)
 
   }
 
--- recipe course project/recipe-book/src/app/shopping-list/shopping-list.service.ts
+++ recipe course project/recipe-book/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,17 @@
-export class ShoppingListService {
+import { Ingredient } from "../shared/ingredient.model"
 
+export class ShoppingListService {
+    // manage shopping list and ingredient
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+    ]
+
+    getIngredients() {
+        return this.ingredients.slice()
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient)
+    }
 }
